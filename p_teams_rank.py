fc550be4dac2ad8576d3a17f167e87f1a87252c1
Eason Ji <a0984348986@gmail.com>
2024-05-19 13:50:51 +0800

    update

--- p_teams_rank.py
+++ p_teams_rank.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 
-url = f'https://pleagueofficial.com/standings'
+url = f'https://pleagueofficial.com/standings/2020-21'
 # 模仿使用者使用發出一個請求到伺服器端請求網頁
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
 response = requests.get(url, headers=headers)
@@ -14,12 +14,13 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 # 排名標題
 data = [th.text for th in soup.find_all('tr')]
+print(data)
 temp_header = data[0].split('\n')
 header = temp_header[1:9]
-# print(header)
+print(header)
 
 body = []
-for i in range(1,7):
+for i in range(1,5):
   temp_body = data[i].split('\n')
   temp_body = temp_body[1:9]
   body = body + [temp_body]
--- season_players_performance.py
+++ season_players_performance.py
@@ -1,18 +1,30 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import numpy as np
+# import requests
+# from bs4 import BeautifulSoup
+# import pandas as pd
+# import numpy as np
+from datetime import datetime
 
-# date = 
-url = f'https://pleagueofficial.com/stat-player/2022-23/2#record'
+# # date = 
+# url = f'https://pleagueofficial.com/stat-player/2022-23/2#record'
 
 
-# 模仿使用者使用發出一個請求到伺服器端請求網頁
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
-response = requests.get(url, headers=headers)
-# print(response.text)
+# # 模仿使用者使用發出一個請求到伺服器端請求網頁
+# headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
+# response = requests.get(url, headers=headers)
+# # print(response.text)
 
-soup = BeautifulSoup(response.text, "html.parser")
+# soup = BeautifulSoup(response.text, "html.parser")
 
-data = [th.text.strip('\n').split() for th in soup.find_all('tr')]
-# print(data)
+# data = [th.text.strip('\n').split() for th in soup.find_all('tr')]
+# temp = data[1:]
+# date = datetime.strptime(temp[1][4], "%M:%S").time()
+# print(date)
+
+def get_time(time_str):
+    try:
+        temp = datetime.strptime(time_str, "%M:%S").time()
+    except ValueError:
+        temp = time_str+'00'
+        temp = datetime.strptime(temp, '%M:%S').time()
+    return temp
+print(get_time('33:'))
